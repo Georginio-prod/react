905830a722c1ab490d9c44056e6864a04c1262ed
Georginio <etonameklou6@gmail.com>
2023-10-23 20:50:41 +0000

    sept

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Calendar from "./scenes/calendar";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
-//import Line from "./scenes/line";
-//import Pie from "./scenes/pie";
-//import Geography from "./scenes/geography";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 
@@ -38,10 +38,9 @@ function App() {
                             <Route path="/calendar" element={<Calendar />} />
                             <Route path="/faq" element={<FAQ />} />
                             <Route path="/bar" element={<Bar />} />
-                            {/*
                             <Route path="/pie" element={<Pie />} />
                             <Route path="/line" element={<Line />} />
-                            <Route path="/geography" element={<Geography />} />*/}
+                            <Route path="/geography" element={<Geography />} />
                         </Routes>
                     </main>
                 </div>
